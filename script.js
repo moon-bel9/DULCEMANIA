8aae1ee0072ad5cc697da36a0289bb4c5e5528f3
Belinda <aquispequezada@gmail.com>
2025-01-29 21:04:31 -0500

    Actualización de index.html script.js ahora si funciona el menu despegable en cel

--- script.js
+++ script.js
@@ -4,10 +4,3 @@ document.getElementById('menu-toggle').addEventListener('click', function() {
 });
 
 
- // Toggle mobile menu
- const mobileMenuToggle = document.getElementById('mobile-menu-toggle');
- const mobileMenu = document.getElementById('mobile-menu');
-
- mobileMenuToggle.addEventListener('click', () => {
-     mobileMenu.classList.toggle('hidden');
- });
