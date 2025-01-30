537f7cadc7b604463ec6561090469a5a5f87200f
Belinda <aquispequezada@gmail.com>
2025-01-29 20:18:37 -0500

    Actualización de script.js cambio de scrip no funciona el boton de menu en el celular

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Alterna la visibilidad del menú en desktop (si es necesario)
-    document.getElementById('menu-toggle').addEventListener('click', function() {
-        const menu = document.getElementById('menu');
-        menu.classList.toggle('hidden');
-    });
-
-    // Alterna la visibilidad del menú móvil al hacer clic en el ícono de las tres rayas
-    const mobileMenuToggle = document.getElementById('mobile-menu-toggle');
-    const mobileMenu = document.getElementById('mobile-menu');
-
-    mobileMenuToggle.addEventListener('click', () => {
-        mobileMenu.classList.toggle('hidden'); // Alterna la clase 'hidden' para mostrar/ocultar el menú
-    });
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('hidden');
 });
+
+
+ // Toggle mobile menu
+ const mobileMenuToggle = document.getElementById('mobile-menu-toggle');
+ const mobileMenu = document.getElementById('mobile-menu');
+
+ mobileMenuToggle.addEventListener('click', () => {
+     mobileMenu.classList.toggle('hidden');
+ });
